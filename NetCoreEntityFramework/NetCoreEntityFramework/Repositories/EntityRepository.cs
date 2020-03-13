51539f2658e7517d70a5a3e0647870007c80fdea
Morten Turn Pedersen <motp@nodes.dk>
2020-03-13 12:50:04 +0100

    More functionality

---
+++ NetCoreEntityFramework/NetCoreEntityFramework/Repositories/EntityRepository.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NetCoreEntityFramework.Models;
+using System;
+using System.Threading.Tasks;
+
+namespace NetCoreEntityFramework.Repositories
+{
+    public abstract class EntityRepository<T> where T : EntityBase
+    {
+        protected DbSet<T> Table { get; private set; }
+
+        protected EntityRepository(DbSet<T> table)
+        {
+            Table = table;
+        }
+
+        public async Task<T> Get(Guid id)
+        {
+            return await Table.FirstOrDefaultAsync(entity => !entity.Deleted && entity.Id == id);
+        }
+    }
+}
