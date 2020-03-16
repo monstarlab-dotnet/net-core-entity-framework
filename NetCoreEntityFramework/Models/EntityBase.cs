00164f5d64660df5c8fd612de1125b94fb6bd490
Morten Turn Pedersen <motp@nodes.dk>
2020-03-16 09:52:54 +0100

    Added more functionality to the entity repository

--- NetCoreEntityFramework/Models/EntityBase.cs
+++ NetCoreEntityFramework/Models/EntityBase.cs
@@ -20,33 +20,5 @@ namespace Nodes.NetCore.EntityFramework.Models
         public DateTime? DeletedAt { get; set; }
 
         public bool Deleted { get; set; }
-
-        public void Delete()
-        {
-            Deleted = true;
-            DeletedAt = DateTime.UtcNow;
-        }
-
-        /// <summary>
-        /// Updates the database correctly
-        /// </summary>
-        /// <typeparam name="T">Should be same type as the model class</typeparam>
-        /// <param name="table">The database table to delete the instance from</param>
-        public void Delete<T>(DbSet<T> table) where T : EntityBase
-        {
-            if (typeof(T).FullName != GetType().FullName)
-            {
-                throw new ArgumentException($"Tried updating a table of type {typeof(T).FullName}. Expected type {GetType().FullName}", nameof(table));
-            }
-
-            Delete();
-
-            table.Update(this as T);
-        }
-
-        public void Restore()
-        {
-            Deleted = false;
-        }
     }
 }
