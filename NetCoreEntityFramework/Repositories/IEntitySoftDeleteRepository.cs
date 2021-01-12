429ca0ffe86110945ebba360b7a50440342c83c0
Morten Turn Pedersen <motp@nodes.dk>
2021-01-12 14:25:30 +0100

    Implemented select for soft delete repository

--- NetCoreEntityFramework/Repositories/IEntitySoftDeleteRepository.cs
+++ NetCoreEntityFramework/Repositories/IEntitySoftDeleteRepository.cs
@@ -42,8 +42,44 @@ namespace Nodes.NetCore.EntityFramework.Repositories
         /// <param name="orderByExpression">The expression to order by.</param>
         /// <param name="orderBy">To order by ascending or descending.</param>
         /// <param name="mode">Whether to include deleted or not.</param>
-        /// <exception cref="ArgumentException"></exception>
         Task<IEnumerable<TEntity>> GetList(
+            Expression<Func<TEntity, bool>> where = null,
+            Expression<Func<TEntity, object>> orderByExpression = null,
+            OrderBy orderBy = OrderBy.Ascending,
+            GetListMode mode = GetListMode.ExcludeDeleted);
+
+        /// <summary>
+        /// Get multiple entities paginated and translated.
+        /// </summary>
+        /// <typeparam name="TResult">The type to return.</typeparam>
+        /// <param name="select">The select statement to get <typeparamref name="TResult"/>.</param>
+        /// <param name="page">Which page to fetch (1 and above).</param>
+        /// <param name="pageSize">The size of each page (1 and above).</param>
+        /// <param name="where">The filter expression.</param>
+        /// <param name="orderByExpression">The expression to order by.</param>
+        /// <param name="orderBy">To order by ascending or descending.</param>
+        /// <param name="mode">Whether to include deleted or not.</param>
+        /// <exception cref="ArgumentException"></exception>
+        Task<IEnumerable<TResult>> GetListWithSelect<TResult>(
+            Expression<Func<TEntity, TResult>> select,
+            [Range(1, int.MaxValue)] int page,
+            [Range(1, int.MaxValue)] int pageSize,
+            Expression<Func<TEntity, bool>> where = null,
+            Expression<Func<TEntity, object>> orderByExpression = null,
+            OrderBy orderBy = OrderBy.Ascending,
+            GetListMode mode = GetListMode.ExcludeDeleted);
+
+        /// <summary>
+        /// Get multiple entities translated.
+        /// </summary>
+        /// <typeparam name="TResult">The type to return.</typeparam>
+        /// <param name="select">The select statement to get <typeparamref name="TResult"/>.</param>
+        /// <param name="where">The filter expression.</param>
+        /// <param name="orderByExpression">The expression to order by.</param>
+        /// <param name="orderBy">To order by ascending or descending.</param>
+        /// <param name="mode">Whether to include deleted or not.</param>
+        Task<IEnumerable<TResult>> GetListWithSelect<TResult>(
+            Expression<Func<TEntity, TResult>> select,
             Expression<Func<TEntity, bool>> where = null,
             Expression<Func<TEntity, object>> orderByExpression = null,
             OrderBy orderBy = OrderBy.Ascending,
