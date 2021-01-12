1bed0fad3820998a128b22a300d8cc61fb0d38d2
Morten Turn Pedersen <motp@nodes.dk>
2021-01-12 14:19:20 +0100

    Implemented select statement in base repository

--- NetCoreEntityFramework/Repositories/EntityRepository.cs
+++ NetCoreEntityFramework/Repositories/EntityRepository.cs
@@ -37,6 +37,21 @@ namespace Nodes.NetCore.EntityFramework.Repositories
             return await query.ToListAsync();
         }
 
+        public async virtual Task<IEnumerable<TResult>> GetListWithSelect<TResult>(
+            Expression<Func<TEntity, TResult>> select,
+            [Range(1, int.MaxValue)] int page,
+            [Range(1, int.MaxValue)] int pageSize,
+            Expression<Func<TEntity, bool>> where = null,
+            Expression<Func<TEntity, object>> orderByExpression = null,
+            OrderBy orderBy = OrderBy.Ascending)
+        {
+            IQueryable<TEntity> query = GetQueryable(where, orderByExpression, orderBy);
+
+            query = Paginate(query, page, pageSize);
+
+            return await query.Select(select).ToListAsync();
+        }
+
         public async virtual Task<IEnumerable<TEntity>> GetList(
             Expression<Func<TEntity, bool>> where = null,
             Expression<Func<TEntity, object>> orderByExpression = null,
@@ -45,6 +60,17 @@ namespace Nodes.NetCore.EntityFramework.Repositories
             IQueryable<TEntity> query = GetQueryable(where, orderByExpression, orderBy);
 
             return await query.ToListAsync();
+        }
+
+        public async virtual Task<IEnumerable<TResult>> GetListWithSelect<TResult>(
+            Expression<Func<TEntity, TResult>> select,
+            Expression<Func<TEntity, bool>> where = null,
+            Expression<Func<TEntity, object>> orderByExpression = null,
+            OrderBy orderBy = OrderBy.Ascending)
+        {
+            IQueryable<TEntity> query = GetQueryable(where, orderByExpression, orderBy);
+
+            return await query.Select(select).ToListAsync();
         }
 
         public virtual Task Add(TEntity entity)
